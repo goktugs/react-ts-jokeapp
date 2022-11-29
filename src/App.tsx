7bc75f146417bfd290690a91fc0f750d77d07b6c
Göktuğ <gokojapan5@gmail.com>
2022-11-29 03:02:20 +0300

    Start Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" font-bold font-quiksand ">hello</h1>
+    </div>
+  );
+}
+
+export default App;
